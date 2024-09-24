552297d7a3770dae09900e7320129f8709181099
Chakkor Bakr <bakr.chakk@gmail.com>
2024-09-24 17:13:51 +0200

    One hundred percent

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,11 +13,28 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_accessor :messages
   def initialize(target_object)
     @object = target_object
+    @methods = @object.methods
+    @messages = []
     # ADD MORE CODE HERE
   end
+  def method_missing(method_name, *args, &block)
+    @messages << method_name
+    if @methods.include?(method_name)
+      @object.__send__(method_name, *args, &block)
+    else super(method_name, *args, &block) end
+  end
 
+    #assert fido.methods.size > 0
+  def called?(method_name)
+    @messages.include?(method_name)
+  end
+
+  def number_of_times_called(method_name)
+    @messages.count(method_name)
+  end
   # WRITE CODE HERE
 end
 
